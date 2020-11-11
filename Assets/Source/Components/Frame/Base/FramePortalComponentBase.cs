06a60f93df7bc8f01a1d22a5ded2279fe1ac652c
Jordan Bleu <jordanbleu1@gmail.com>
2020-11-11 07:38:46 -0600

    Added loaded level frame areas

---
+++ Assets/Source/Components/Frame/Base/FramePortalComponentBase.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Source.Components.Frame.Base
+{
+    public abstract class FramePortalComponentBase : ComponentBase
+    {
+
+        [SerializeField]
+        [Tooltip("The frame we are transitioning from")]
+        protected GameObject SourceFrame;
+
+        [SerializeField]
+        [Tooltip("The frame we are transitioning to")]
+        protected GameObject DestinationFrame;
+
+        [SerializeField]
+        [Tooltip("The starting position for the player within the destination frame")]
+        protected Vector3 StartPosition;
+
+    }
+}
---
+++ Assets/Source/Components/Frame/FrameComponent.cs
@@ -0,0 +1,11 @@
+﻿namespace Assets.Source.Components.Frame
+{
+    /// <summary>
+    /// Frames are small loaded chunks of the level.  This prevents overuse of system resources by updating entire massive levels, 
+    /// as well as reduces required load times from loading new scenes.
+    /// </summary>
+    public class FrameComponent : ComponentBase
+    {
+        // Currently Does nothing
+    }
+}
